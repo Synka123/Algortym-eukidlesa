80dc43b61e4b551546bb491a85f4c4ffba5f0eac
Szymon Harpula <97166816+Synka123@users.noreply.github.com>
2022-06-09 20:26:33 +0200

    Update nwd.py

--- nwd.py
+++ nwd.py
@@ -6,3 +6,17 @@ while x>y or x<y:
     elif x<y:
         y=y-x
 print(x)
+
+#podejście rekurencyjne
+def nwd(x, y):
+    if x==y:
+        return x
+    if x>y:
+        return nwd(x - y, y)
+    return nwd(x,y - x)
+
+
+i=int(input("podaj pierwszą liczbe:"))
+j=int(input("podaj drugą liczbe:"))
+r = nwd(i,  j)
+print(r)
